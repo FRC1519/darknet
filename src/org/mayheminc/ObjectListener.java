a34ec8a04bb6f513f75e6597eaed98d675740c98
Bob Bell <bob.bell@mayheminc.org>
2018-02-26 22:30:48 -0500

    Initialize an empty list in Java listener

--- src/org/mayheminc/ObjectListener.java
+++ src/org/mayheminc/ObjectListener.java
@@ -35,6 +35,7 @@ public class ObjectListener extends Thread {
         byte[] byteBuffer = new byte[MAX_BUFFER];
         packet = new DatagramPacket(byteBuffer, byteBuffer.length);
         buffer = ByteBuffer.wrap(byteBuffer);
+        objList = new ArrayList<ObjectListener>();
     }
 
     public int getLastFrame() {
