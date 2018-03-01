69a20b30cb86d4c638d7b5ab641bda959de8877f
Bob Bell <bob.bell@mayheminc.org>
2018-03-01 17:15:35 -0500

    Checkpoint progress experimenting with drawing detected boxes

--- testing.cpp
+++ testing.cpp
@@ -1,48 +1,167 @@
 // g++ -Og -o testing `pkg-config --cflags opencv` testing.cpp `pkg-config --libs opencv` && ./testing
+// g++ -Og -o testing `pkg-config --cflags opencv` testing.cpp -static `pkg-config --libs opencv libv4l1 libv4l2 libv4lconvert libjpeg libgphoto2 libgphoto2_port libtiff-4 liblzma zlib libpng libwebp libexif libdc1394-2 gstreamer-1.0` -pthread -ldl -ljbig -lltdl 
+
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <inttypes.h>
+#include <endian.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <opencv2/opencv.hpp>
+
+#include "robot.h"
 
 using namespace std;
 using namespace cv;
 
-#define CAM_CAP "video/x-raw"
-#define STREAM_DEST_HOST "192.168.0.252"
-#define STREAM_DEST_PORT "9999"
-//#define GSTREAMER_COMMAND "videotestsrc ! progressreport update-freq=1 ! " CAM_CAP " ! tee name=t ! queue ! videoconvert ! queue ! jpegenc ! rtpjpegpay ! udpsink host=" STREAM_DEST_HOST " port=" STREAM_DEST_PORT " t. ! videoconvert ! appsink name=opencvsink"
-#define GSTREAMER_COMMAND "multifilesrc location=/home/bbell/capimg/frame_\%04d.jpg caps=\"image/jpeg, framerate=30/1\" ! jpegdec ! videoconvert ! appsink"
-//#define GSTREAMER_COMMAND "videotestsrc ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! appsink"
+#define GSTREAMER_COMMAND "multifilesrc location=/home/bbell/capimg/frame_\%04d.jpg caps=\"image/jpeg, framerate=15/1\" ! jpegdec ! videoconvert ! appsink"
 
-const char *windowName;
 const char *windowTitle = "My Testing";
 int wait_ms = 10;
 int thickness = 2;
 int lineType = LINE_AA;
-Scalar colors[] = {
-    Scalar(0, 255, 255), // Cube
-    Scalar(255, 0, 0), // Red bumper
-    Scalar(0, 0, 255), // Blue bumper
-};
+
+pthread_mutex_t obj_lock  = PTHREAD_MUTEX_INITIALIZER;
+int obj_frame = 0;
+int obj_count = 0;
+long obj_timestamp = 0;
+object_location obj_loc[MAX_OBJECTS_PER_FRAME];
+
+float minThreshold = 0.15; // Minimum probability to draw
+float maxThreshold = 0.75; // Probability above which we consider object basically certain
+float dimmingRange = 0.3; // Amount of color that is subject to dimming
 
 Rect ourRect(float x, float y, float w, float h, Size size) {
-    float left  = (x - w/2.) * size.width;
-    float right = (x + w/2.) * size.height;
+    float left = (x - w / 2.) * size.width;
+    float top = (y - h / 2.) * size.height;
     float width = w * size.width;
     float height = h * size.height;
 
-    return Rect(left, right, width, height);
+    return Rect(left, top, width, height);
 }
 
+void *listener(void *arg) {
+    int port = 5810;
+    int sock;
+    int reuseaddr;
+    struct sockaddr_in svr_addr;
+    int rv;
+    ssize_t n;
+    datagram data;
 
-int main(int argc, char **argv)
-{
-    Size size = Size(0, 0);
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("Failed to create datagram socket");
+        exit(1);
+    }
 
-    stringstream ss;
-    ss << argv[0] << "[" << getpid() << "]";
-    windowName = ss.str().c_str();
+    rv = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
+    if (rv < 0) {
+        perror("Failed to configure socket to reuse address");
+        exit(1);
+    }
 
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    svr_addr.sin_port = htons(port);
+
+    rv = bind(sock, (struct sockaddr *)&svr_addr, sizeof(svr_addr));
+    if (rv < 0) {
+        perror("Failed to bind to server port");
+        exit(1);
+    }
+
+    printf("Awaiting notification of objects...\n");
+    while (1) {
+        n = recv(sock, &data, sizeof(data), 0);
+        if (n < 0) {
+            perror("Failed receiving datagram");
+            exit(1);
+        }
+        if (n != sizeof(data)) {
+            fprintf(stderr, "WARNING: received %zd bytes instead of %zu bytes\n", n, sizeof(data));
+            continue;
+        }
+        if (be32toh(data.magic) != MAYHEM_MAGIC) {
+            fprintf(stderr, "WARNING: malformed datagram received\n");
+            continue;
+        }
+
+        pthread_mutex_lock(&obj_lock);
+        obj_frame = be32toh(data.frame_number);
+        obj_timestamp = be64toh(data.timestamp);
+        cout << "Received frame " << obj_frame << " sent at " << obj_timestamp << endl;
+
+        for (obj_count = 0; obj_count < MAX_OBJECTS_PER_FRAME; obj_count++) {
+            obj_loc[obj_count].type = (object_type)be32toh(data.object_data[obj_count].type);
+            if (obj_loc[obj_count].type == OBJ_NONE)
+                break;
+            obj_loc[obj_count].x = (float)be32toh(data.object_data[obj_count].x) / INT32_MAX;
+            obj_loc[obj_count].y = (float)be32toh(data.object_data[obj_count].y) / INT32_MAX;
+            obj_loc[obj_count].width = (float)be32toh(data.object_data[obj_count].width) / INT32_MAX;
+            obj_loc[obj_count].height = (float)be32toh(data.object_data[obj_count].height) / INT32_MAX;
+            obj_loc[obj_count].probability = (float)be32toh(data.object_data[obj_count].probability) / INT32_MAX;
+            printf("OBJECT FOUND in frame #%" PRIu32 ": Type %d @ %.02f x %.02f [ %.02f x %.02f ], %.02f%%\n", obj_frame, obj_loc[obj_count].type, obj_loc[obj_count].x, obj_loc[obj_count].y, obj_loc[obj_count].width, obj_loc[obj_count].height, obj_loc[obj_count].probability);
+        }
+        pthread_mutex_unlock(&obj_lock);
+    }
+}
+
+// Pick a color based on the object type a probability
+Scalar pickColor(object_type objType, float probability) {
+    int type = (int)objType;
+    Scalar color = Scalar(150, 150, 150); // Default to a medium gray
+
+    // High certain object types with associated colors
+    switch (objType) {
+        case OBJ_CUBE: color = Scalar(0, 255, 255); break;
+        case OBJ_BUMPERS_RED: color = Scalar(255, 0, 0); break;
+        case OBJ_BUMPERS_BLUE: color = Scalar(0, 0, 255); break;
+        case OBJ_BOX: color = Scalar(0, 255, 0); break;
+        default: break;
+    }
+
+    // Adjust intensity of color based on probability
+    if (probability < maxThreshold)
+        color *= 1.0 - (1.0 - ((probability - minThreshold) / (maxThreshold - minThreshold))) * dimmingRange;
+
+    return color;
+}
+
+void draw_objs(Mat &frame) {
+    static Size size = Size(0, 0);
+
+    if (size.width == 0)
+        size = frame.size();
+
+    pthread_mutex_lock(&obj_lock);
+    for (int i = 0; i < obj_count; i++) {
+        // Skip unlikely objects
+        if (obj_loc[i].probability < minThreshold)
+            continue;
+
+        printf("DRAWING OBJECT in frame #%" PRIu32 ": Type %d @ %.02f x %.02f [ %.02f x %.02f ], %.02f%%\n", obj_frame, obj_loc[i].type, obj_loc[i].x, obj_loc[i].y, obj_loc[i].width, obj_loc[i].height, obj_loc[i].probability);
+
+        Scalar color = pickColor(obj_loc[i].type, obj_loc[i].probability);
+
+        rectangle(frame, ourRect(obj_loc[i].x, obj_loc[i].y, obj_loc[i].width, obj_loc[i].height, size), color, thickness, LINE_AA);
+    }
+    pthread_mutex_unlock(&obj_lock);
+}
+
+int main(int argc, char **argv) {
+    // Listen for detected object information in a separate thread
+    pthread_t listener_thread;
+    cout << "Starting listener..." << endl;
+    if (pthread_create(&listener_thread, NULL, listener, NULL)) {
+        cerr << "Thread creation failed" << endl;
+        exit(1);
+    }
+
+    // Open a connection to the video feed
     cout << "Connecting to GStreamer (" GSTREAMER_COMMAND ")..." << endl;
     VideoCapture cap(GSTREAMER_COMMAND, CV_CAP_GSTREAMER);
     if (!cap.isOpened()) {
@@ -51,32 +170,38 @@ int main(int argc, char **argv)
     }
     cout << "Connected to GStreamer" << endl;
 
-    namedWindow(windowName, WINDOW_FREERATIO);
+    // Build a unique window name
+    char windowName[30];
+    snprintf(windowName, sizeof(windowName), "WINDOW[%d]", getpid());
+
+    // Create window to display video feed
+    namedWindow(windowName, WINDOW_KEEPRATIO);
     setWindowTitle(windowName, windowTitle);
 
     Mat frame;
     while (true) {
+        // Obtain next frame (blocks waiting for frame)
         cap >> frame;
-        if (frame.empty()) {
+
+        // Abort if feed has terminated
+        if (frame.empty())
             break;
-        }
 
-        if (size.width == 0)
-            size = frame.size();
+        // Augment frame with most recent knowledge of images
+        draw_objs(frame);
 
-        Rect myRect = ourRect(0.5, 0.75, 0.25, 0.35, size);
-
-        rectangle(frame, myRect, colors[0], thickness, LINE_AA);
-
+        // Show the augmented frame in the window
         imshow(windowName, frame);
 
+        // Abort if escape key has been hit
         int key = waitKey(wait_ms);
         if ((key & 0xFF) == 27)
             break;
+
+        // Abort if window has been closed
         if (getWindowProperty(windowName, WND_PROP_ASPECT_RATIO) < 0)
             break;
     }
 
-    cout << "Exiting" << endl;
     exit(0);
 }
